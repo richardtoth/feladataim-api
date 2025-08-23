40035cb5331e1f0a0a4dae666eefbf386d7e3358
Richard Toth <richardtothhu@gmail.com>
2025-08-23 14:08:27 +0200

    add health check route with app and database status response

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+Route::get('/health', static function () {
+    try {
+        DB::connection()->getPdo();
+        $databaseStatus = 'ok';
+    } catch (\Exception $e) {
+        $databaseStatus = 'error';
+    }
+
+    return Response::json([
+        'app_status' => 'ok',
+        'database_status' => $databaseStatus
+    ]);
+})->middleware('throttle:10,1');
